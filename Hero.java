6cafa840d4d6fa5dd053858afe725159d0b6c092
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-20 18:40:31 +0100

    Added "up" movement to Hero

--- Hero.java
+++ Hero.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Hero {
     char znak = 'H';
@@ -10,5 +11,15 @@ public class Hero {
     }
     public int getAttributes(String attribute){
         return attributes.getOrDefault(attribute,0);
+    }
+    public void heroMovement(char[][] board,Hero hero){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose letter");
+        System.out.println("W - up A- left S- down D-right");
+        char move = sc.next().charAt(0);
+        if (move == 'W' || move == 'w') {
+            board[hero.pozycjaX][hero.pozycjaY] = board[hero.pozycjaX-1][hero.pozycjaY];
+            board[hero.pozycjaX-1][hero.pozycjaY] = hero.znak;
+        }
     }
 }
