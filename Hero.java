0f31fdaa400b75297fd3042abb79b94eddfb834e
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-21 11:46:50 +0100

    Added Hero movement
    -Added movement using switch case

--- Hero.java
+++ Hero.java
@@ -6,20 +6,51 @@ public class Hero {
     int pozycjaX;
     int pozycjaY;
     HashMap<String, Integer> attributes = new HashMap<>();
-    public void setAttributes(String attribute, int value){
-        attributes.put(attribute,value);
+    public void setAttributes(String attribute, int value) {
+        attributes.put(attribute, value);
     }
-    public int getAttributes(String attribute){
-        return attributes.getOrDefault(attribute,0);
+    public int getAttributes(String attribute) {
+        return attributes.getOrDefault(attribute, 0);
     }
-    public void heroMovement(char[][] board,Hero hero){
+    public void heroMovement(char[][] board, Hero hero) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose letter");
         System.out.println("W - up A- left S- down D-right");
         char move = sc.next().charAt(0);
-        if (move == 'W' || move == 'w') {
-            board[hero.pozycjaX][hero.pozycjaY] = board[hero.pozycjaX-1][hero.pozycjaY];
-            board[hero.pozycjaX-1][hero.pozycjaY] = hero.znak;
+        try {
+            switch (move){
+                case 'W':
+                case 'w':
+                        board[hero.pozycjaX][pozycjaY] = '.';
+                        hero.pozycjaX = hero.pozycjaX - 1;
+                        board[hero.pozycjaX][hero.pozycjaY] = hero.znak;
+                        break;
+                case 'A':
+                case 'a':
+
+                        board[hero.pozycjaX][pozycjaY] = '.';
+                        hero.pozycjaY = hero.pozycjaY - 1;
+                        board[hero.pozycjaX][hero.pozycjaY] = hero.znak;
+                        break;
+                case 'S':
+                case 's':
+                        board[hero.pozycjaX][pozycjaY] = '.';
+                        hero.pozycjaX = hero.pozycjaX + 1;
+                        board[hero.pozycjaX][hero.pozycjaY] = hero.znak;
+                        break;
+                case 'D':
+                case 'd':
+                        board[hero.pozycjaX][pozycjaY] = '.';
+                        hero.pozycjaY = hero.pozycjaY + 1;
+                        board[hero.pozycjaX][hero.pozycjaY] = hero.znak;
+                        break;
+                default:
+                        System.out.println("No action assigned to this key");
+                        break;
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("Hero out of bounds exception");
+                }
+            }
         }
-    }
-}
+
