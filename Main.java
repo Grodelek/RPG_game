7b506391d13f84ee5d68e5161d8a98f880beb9a5
A.Grodel <98063447+Grodelek@users.noreply.github.com>
2024-03-22 08:31:34 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,20 +19,29 @@ public class Main {
         //Adding Hero movement
       while(true) {
           hero.heroMovement(board, hero);
+          space();
+          hero.heroStats(hero);
+          hero.getHeroStats();
           printRoom(board);
-
       }
     }
 
     public static void printRoom(char[][] board){
-        for(int i=0; i< board.length; i++){
+         for(int i=0; i< board.length; i++){
             for(int j=0; j< board[i].length; j++){
                 System.out.print(board[i][j]+" ");
             }
             System.out.println();
         }
     }
+    public static void space(){
+        for (int i = 0; i < 6; i++) {
+            System.out.println();
+        }
+    }
 }
+
+
 
 
 
